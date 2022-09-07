bd545c6965f12ed791c21a4472becae9d37f5106
Katya <katvas785@gmail.com>
2022-09-07 20:03:50 +0300

    Ex34

---
+++ Ex34/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[10];
+int y = 0;
+int x = 0;
+while(y <10)
+{
+     array[y] = new Random().Next(100, 1000);
+     Console.WriteLine(array[y]);
+     if(array[y]%2==0)
+     {
+        x++;
+     } 
+     y++;
+}
+Console.WriteLine(x);
