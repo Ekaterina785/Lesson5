bb2064b474ac90dfa17496dc09f9d6db01569a44
Katya <katvas785@gmail.com>
2022-09-07 20:06:22 +0300

    Ex38

---
+++ Ex38/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array = new int[4];
+int y=0;
+int max =-1;
+int min =100;
+while(y<4)
+{
+    array[y] = new Random().Next(1,100);
+    Console.WriteLine(array[y]);
+    if(array[y]>max)
+    {
+        max=array[y];
+    }
+    else
+    {
+        if(array[y]<min)
+        {
+            min=array[y];
+        }
+    }
+    y++;
+}
+Console.Write("Разность ");
+Console.WriteLine(max - min);
