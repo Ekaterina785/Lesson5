da658e337961551c66290ded501d453fdd11fe0a
Katya <katvas785@gmail.com>
2022-09-07 20:05:17 +0300

    Ex36

---
+++ Ex36/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array=new int[10];
+int y=0;
+int sum=0;
+while(y<10)
+{
+    array[y]= new Random().Next(0,100);
+    Console.WriteLine(array[y]);
+    if(y%2!=0)
+    {
+        sum=sum+array[y];
+    }
+    y++;
+    
+}
+Console.Write("Сумма элементов на нечётных позициях ");
+Console.WriteLine(sum);
